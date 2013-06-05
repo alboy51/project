8d725f5e19c0a2f2e5c6e77e5fc0644a007b67be
Alexey <cartman1001@hotmail.com>
2013-06-05 16:22:41 -0400

    Attempting to get GIT to work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = 'c84670bdf247e99e66e5828eaf250a640507b51b1c4a4ae84f822b282909064f6f441c9e9c66ec43a3398361b2e4178b753273375c82316c36e3460e5593414b'
